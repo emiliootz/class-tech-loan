47374a50596643aee48dd600208f7d231ef2d6cd
emiliootz <emiliootz@gmail.com>
2024-11-19 18:32:14 -0500

    setup pages with post and get

--- app.js
+++ app.js
@@ -5,6 +5,36 @@ app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
+app.get('/login', (req, res) => {
+    res.send("Login Get")
+})
+
+app.post('/login', (req, res) => {
+    res.send("Login Post")
+})
+
+app.get('/register', (req, res) => {
+    res.send("Register Get")
+})
+
+app.post('/register', (req, res) => {
+    res.send("Register Post")
+})
+
+app.get('/logout', (req, res) => {
+    res.send("logout Get")
+})
+
+app.post('/logout', (req, res) => {
+    res.send("logout Post")
+})
+
+app.get('/protected', (req, res) => {
+    res.send("protected Get")
+})
+
+
+
 app.listen(3000, (req, res) => {
     console.log("listening to port 3000");
 })
