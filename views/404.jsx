b371de56fe7ad19eeb254f791cdfcf8011e52cf2
emiliootz <emiliootz@gmail.com>
2025-01-26 14:54:08 -0500

    update 404 page

--- views/404.jsx
+++ views/404.jsx
@@ -6,19 +6,17 @@ function NotFoundPage({ isLoggedIn, cartCount }) {
     <>
       <head>
         <title>404 Not Found</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-        />
-        <link rel="stylesheet" href="/css/styles.css" />
+        <link rel="stylesheet" href="/css/404.css" />
       </head>
 
       <Navbar isLoggedIn={isLoggedIn} cartCount={cartCount} />
 
-      <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
-        <h1 className="display-1">404</h1>
-        <p className="lead">Oops! The page you're looking for doesn't exist.</p>
-        <a href="/" className="btn btn-primary mt-3">
+      <div className="not-found-container">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-message">
+          Oops! The page you're looking for doesn't exist.
+        </p>
+        <a href="/" className="not-found-btn">
           Go Back Home
         </a>
       </div>
