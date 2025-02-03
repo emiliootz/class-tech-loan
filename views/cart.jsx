beea5468086eeef612cbb87afbd71edf10e7a0d7
emiliootz <emiliootz@gmail.com>
2025-02-03 14:20:18 -0500

    updated cart page and comments

--- views/cart.jsx
+++ views/cart.jsx
@@ -1,16 +1,38 @@
+/**
+ * @file CartPage.jsx
+ * @description Displays the shopping cart page, allowing users to review and remove items,
+ *              and select arrival and return times for borrowed equipment.
+ * @module CartPage
+ */
+
 const React = require("react");
-const Navbar = require("./components/Navbar");
-const ItemDetails = require("./components/ItemDetails");
+const Navbar = require("./components/Navbar"); // Importing the navigation bar component
+const TimePicker = require("./components/TimePicker"); // Importing the time selection component
 const {
   RemoveButton,
   CheckoutButton,
   HomeButton,
-} = require("./components/Buttons");
+} = require("./components/Buttons"); // Importing buttons for cart actions
 
+/**
+ * CartPage Component
+ *
+ * This component renders the cart page where users can:
+ *  - View a list of items added to their cart
+ *  - Remove items from the cart
+ *  - Select an arrival and return time for borrowed equipment
+ *  - Proceed to checkout
+ *
+ * @param {Object} props - Component props
+ * @param {Array} props.cartItems - List of items currently in the user's cart
+ * @param {boolean} props.isLoggedIn - Indicates whether the user is logged in
+ * @param {number} props.cartCount - Number of items in the cart (used in navbar)
+ * @param {boolean} [props.isAdmin=false] - Determines if the user has admin privileges
+ * @param {boolean} [props.isStaff=false] - Determines if the user is a staff member
+ * @returns {JSX.Element} The Cart Page UI
+ */
 function CartPage({
   cartItems,
-  handleDelete,
-  handleCheckout,
   isLoggedIn,
   cartCount,
   isAdmin = false,
@@ -18,23 +40,34 @@ function CartPage({
 }) {
   return (
     <>
+      {/* Head section for page-specific styles */}
       <head>
         <link rel="stylesheet" href="/css/cart.css" />
       </head>
+
+      {/* Navbar for site-wide navigation */}
       <Navbar
         cartCount={cartCount}
         isLoggedIn={isLoggedIn}
         isAdmin={isAdmin}
         isStaff={isStaff}
       />
+
+      {/* Main cart container */}
       <section className="container">
+        {/* Left container: Displays cart items and actions */}
         <div className="left-container">
           <h1>Your Cart</h1>
+
+          {/* Display items in cart or show empty message */}
           {cartItems.length > 0 ? (
             <ul className="items">
               {cartItems.map((item) => (
                 <li key={item._id} className="single-item">
-                  <ItemDetails item={item} fields={["Make", "Model"]} />
+                  <img src={item.picture} alt={`${item.make} ${item.model}`} />
+                  <h1>
+                    {item.make} {item.model}
+                  </h1>
                   <div className="remove-button">
                     <RemoveButton itemId={item._id} />
                   </div>
@@ -42,9 +75,10 @@ function CartPage({
               ))}
             </ul>
           ) : (
-            <p className="cart-empty">Your cart is empty!</p>
+            <p>Your cart is empty!</p>
           )}
 
+          {/* Display checkout and continue shopping buttons only if cart is not empty */}
           {cartItems.length > 0 && (
             <div className="cart-actions">
               <HomeButton text="Continue Shopping" link="/" />
@@ -53,13 +87,16 @@ function CartPage({
           )}
         </div>
 
-        {/* Right side - Time Display */}
+        {/* Right container: Time selection for arrival and return */}
         <div className="right-container">
-          <div className="time-message">Time goes here</div>
+          <div className="times">
+            <TimePicker label="Arrival Time" id="arrivalDate" />
+            <TimePicker label="Return Time" id="returnDate" />
+          </div>
         </div>
       </section>
     </>
   );
 }
 
-module.exports = CartPage;
+module.exports = CartPage; // Exporting the CartPage component for use in routing.
